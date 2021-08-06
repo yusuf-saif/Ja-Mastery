fa7b0fa90becdb87bc9b3e2d18a9b166374dc24d
yusuf-saif <ysaifullah28kbg.com>
2021-08-06 15:34:32 +0100

    changes

---
+++ dom.js
@@ -0,0 +1,7 @@
+//GET ELEMENT BY ID
+//console.log(document.getElementById0('header-title'));
+var headerTitle = document.getElementById('header-title')
+console.log(headerTitle);
+headerTitle.textContent = 'Hello';
+headerTitle.innerText = 'Goodbye';
+
