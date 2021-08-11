d5c715f200bc77161b9f3d976541e634a00c348a
yusuf-saif <ysaifullah28kbg.com>
2021-08-11 14:27:33 +0100

    new changes

--- dom.js
+++ dom.js
@@ -1,7 +1,14 @@
 //GET ELEMENT BY ID
 //console.log(document.getElementById0('header-title'));
-var headerTitle = document.getElementById('header-title')
-console.log(headerTitle);
-headerTitle.textContent = 'Hello';
-headerTitle.innerText = 'Goodbye';
+// var headerTitle = document.getElementById('header-title')
+// var header = document.getElementById('main-header')
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'Goodbye';
+// headerTitle.innerHTML = '<h2> WELCOME</h2>';
+// header.style.borderBottom = '3px solid #00000';\
 
+//GET ELMENTS BY CLASS NAME:
+var items = document.getElementsByClassName('list-group-item');
+//console.log(items[1]);
+items.style.color = '#000000';
